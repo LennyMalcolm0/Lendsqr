7e0c4611d4738be21c3322bba40744fb75cfa774
Melariri Prosper <leonardprosper00@gmail.com>
2023-04-30 22:34:56 +0100

    Completed design of Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import './styles/index.css'
-import PageHeader from './layouts/PageHeader'
-import SideNavbar from './layouts/sideNavbar'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
 
 function App() {
 
   return (
     <div className="app">
-      {/* <PageHeader />
-      <SideNavbar /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navigate to="/login" />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,27 +1,25 @@
 const Login = () => {
     return (  
-        <div className="login-page">
-            <img src="src\assets\images\lendsqr-logo.svg" alt="" />
-            <div className="grid">
-                <div className="image">
-                    <img src="src\assets\images\pablo-sign-in.svg" alt="" />
-                </div>
-                <div>
+        <div className="login-page h-full w-full">
+            <img src="src\assets\images\lendsqr-logo.svg" alt="" className="ml-40 lendsqr-logo ps-absolute top-60 left-20"/>
+            <div className="images h-screen w-50 mt-20">
+                <img src="src\assets\images\pablo-sign-in.svg" alt="" className="login-2d-image" />
+            </div>
+            <div className="form-container h-screen w-50">
+                <form>
                     <h1>Welcome!</h1>
                     <h3>Enter details to login.</h3>
-                    <form>
-                        <input type="text" placeholder="Email" />
-                        <div className="password-input ">
-                            <input type="text" placeholder="Password" />
-                            <span>SHOW</span>
-                        </div>
-                        <span>FORGOT PASSWORD?</span>
-                        <button type="submit">LOG IN</button>
-                    </form>
-                </div>
+                    <input type="text" placeholder="Email" />
+                    <div className="password-input ">
+                        <input type="text" placeholder="Password" />
+                        <span>SHOW</span>
+                    </div>
+                    <span>FORGOT PASSWORD?</span>
+                    <button type="submit" >LOG IN</button>
+                </form>
             </div>
         </div>
     );
 }
- 
+        
 export default Login;
